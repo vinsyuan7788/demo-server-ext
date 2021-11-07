7157243097176accd834a4bf84bb1dd7cf6d1bfb
VinceYuan <vinsyuan7788@gmail.com>
2021-11-07 21:34:58 +0800

    Code adjustment

--- demo-server-ext-web/src/main/java/com/demo/server/ext/web/DemoServerExtApplication.java
+++ demo-server-ext-web/src/main/java/com/demo/server/ext/web/DemoServerExtApplication.java
@@ -19,6 +19,6 @@ public class DemoServerExtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoServerExtApplication.class, args);
-        log.info(LogUtils.getLogMessage("Server extensive application is started"));
+        log.info(LogUtils.getLogMessage("Server extensive application has been started successfully"));
     }
 }
